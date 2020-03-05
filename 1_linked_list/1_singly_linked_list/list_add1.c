315572921d8c5a60e99bd76951d04a61a6ae31fa
Steve <steve@Steveui-MacBook-Air.local>
2020-03-05 19:03:53 +0900

    20/03/05

--- 1_linked_list/1_singly_linked_list/list_add1.c
+++ 1_linked_list/1_singly_linked_list/list_add1.c
@@ -3,26 +3,28 @@
 int		list_add1(t_node **begin_list, int data)
 {
 	int		i;
-	t_node	*current;
+	t_node	*curr;
 	t_node	*new;
 
 	if (begin_list == 0 || (new = create_elem(data)) == 0)
 		return (-1);
-	current = *begin_list;
-	if (current == 0)
+	if (*begin_list == 0)
 	{
-		current = create_elem(data);
+		*begin_list = new;
 		return (0);
 	}
-	else
+	curr = *begin_list;
+	i = 1;
+	while (curr->next)
 	{
-		i = 1;
-		while (current->next)
-		{
-			current = current->next;
-			i++;
-		}
-		current->next = new;
+		curr = curr->next;
+		i++;
 	}
+	curr->next = new;
 	return (i);
 }
+
+/*
+** line 14 : curr = new   >>>  *begin_list = new
+** line 14 : begin_list = create_elem(data)   >>>  begin_list = new
+*/
--- 1_linked_list/1_singly_linked_list/main.c
+++ 1_linked_list/1_singly_linked_list/main.c
@@ -1,38 +1,41 @@
 #include "list.h"
 
-int		main(void)
+int main(void)
 {
-	t_node **begin_list = (t_node **)malloc(sizeof(t_node *));
-	printf("size = %d\n", list_size(*begin_list));
+	t_node **begin_list = malloc(sizeof(t_node *));
 
+	*begin_list = 0;
+	printf("------ RESULT : LIST_SIZE ---------\n");
+	printf("size = %d\n", list_size(*begin_list));
 	list_add1(begin_list, 0);
 	printf("size = %d\n", list_size(*begin_list));
-
 	list_add1(begin_list, 1);
 	printf("size = %d\n", list_size(*begin_list));
-
 	list_add1(begin_list, 2);
 	printf("size = %d\n", list_size(*begin_list));
-
 	list_add1(begin_list, 3);
 	printf("size = %d\n", list_size(*begin_list));
 
+	printf("\n------ RESULT : LIST_ADD1 ---------\n");
 	t_node *curr = *begin_list;
 	for (; curr; curr = curr->next)
 	{
 		printf("%d\n", curr->data);
 	}
 
+	printf("\n------ RESULT : LIST_GET ---------\n");
 	for (int i = -2; i < 5; i++)
 	{
 		printf("%p\n", list_get(*begin_list, i));
 	}
 
+	printf("\n------ RESULT : LIST_FIND ---------\n");
 	for (int i = -2; i < 6; i++)
 	{
 		printf("find = %d\n", list_find(*begin_list, i));
 	}
 
+	printf("\n------ RESULT : LIST_REMOVE ---------\n");
 	printf("remove = %d\n", list_remove(begin_list, 3));
 	curr = *begin_list;
 	for (; curr; curr = curr->next)
@@ -40,11 +43,13 @@ int		main(void)
 		printf("%d\n", curr->data);
 	}
 
+	printf("\n------ RESULT : LIST_ADD ---------\n");
 	printf("add -> %d\n", list_add(begin_list, 10, 5));
 	curr = *begin_list;
 	for (; curr; curr = curr->next)
 	{
 		printf("%d\n", curr->data);
 	}
+
 	return (0);
 }
