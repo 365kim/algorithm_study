2ac6676acfc3f9c358f2f70cbbf00c1d21aaae7b
365kim <365listener@gmail.com>
2020-03-24 18:07:39 +0900

    2020. 03. 24. Fixed one of the comparision operands

--- 3_queue/1_queue_using_array/queue_push.c
+++ 3_queue/1_queue_using_array/queue_push.c
@@ -6,7 +6,7 @@
 /*   By: mihykim <mihykim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/24 13:03:56 by mihykim           #+#    #+#             */
-/*   Updated: 2020/03/24 13:12:09 by mihykim          ###   ########.fr       */
+/*   Updated: 2020/03/24 17:59:20 by mihykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,13 @@
 
 int		queue_push(t_queue *queue, void *data)
 {
-	if (queue == NULL || queue->data == NULL ||  queue->last_index >= (int)queue->max_size)
+	if (queue == NULL || queue->data == NULL ||  queue->last_index + 1 >= (int)queue->max_size)
 		return (0);
 	queue->last_index++;
 	queue->data[queue->last_index] = data;
 	return (1);
 }
+
+/*
+** line 17 : 'queue->last_index >= (int)queue->max_size' is wrong because index starts from 0.
+*/
