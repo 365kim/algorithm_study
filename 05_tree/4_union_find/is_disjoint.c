83fa93cb65c9f353b432ff00793fedfa77713ff0
365kim <365listener@gmail.com>
2020-04-23 14:07:44 +0900

    2020. 04. 23. Considered every set is disjoint from the empty set

--- 05_tree/4_union_find/is_disjoint.c
+++ 05_tree/4_union_find/is_disjoint.c
@@ -6,7 +6,7 @@
 /*   By: mihykim <mihykim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/17 00:05:56 by mihykim           #+#    #+#             */
-/*   Updated: 2020/04/17 01:57:30 by mihykim          ###   ########.fr       */
+/*   Updated: 2020/04/23 14:06:57 by mihykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 bool	is_disjoint(t_node *node1, t_node *node2)
 {
-	if (node1 == NULL || node2 == NULL || find(node1) == find(node2))
+	if (node1 == NULL || node2 == NULL)
+		return (true);
+	if (find(node1) == find(node2))
 		return (false);
 	return (true);
 }
 
 /*
-** line 17 : changed from 'node1->parent == node2->parent'
+** line 18 : Changed from 'false' to true,
+**           since every set is disjoint from the empty set.
+** line 19 : Changed from 'node1->parent == node2->parent'
 **           to 'find(node1) == find(node2)'
 */
