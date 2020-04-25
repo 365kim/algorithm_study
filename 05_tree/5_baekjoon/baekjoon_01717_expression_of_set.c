67facc8d2c79ac92774eeaf2f13dc3dfc7a6f179
365kim <365listener@gmail.com>
2020-04-25 10:20:09 +0900

    2020. 04. 25. Solved and graded #1717

--- 05_tree/5_baekjoon/baekjoon_01717_expression_of_set.c
+++ 05_tree/5_baekjoon/baekjoon_01717_expression_of_set.c
@@ -6,7 +6,7 @@
 /*   By: mihykim <mihykim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/23 12:18:03 by mihykim           #+#    #+#             */
-/*   Updated: 2020/04/23 14:01:13 by mihykim          ###   ########.fr       */
+/*   Updated: 2020/04/25 10:16:32 by mihykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,31 +36,27 @@ typedef struct s_node
 	int           key;
 }				t_node;
 
-t_node *init[1000000];
+t_node *init[1000001];
 
 t_node *create_node(int n)
 {
 	t_node *node;
 
 	node = malloc(sizeof(t_node));
-	node->parent = NULL;
+	node->parent = node;
 	node->key = n;
 	return (node);
 }
 
 t_node *to_root(t_node *node)
 {
-	if (node == NULL)
-		return (NULL);
-	while (node->parent)
-		node = node->parent;
-	return (node);
+	if (node->parent == node)
+		return (node);
+	return (node->parent = to_root(node->parent));
 }
 
 int is_disjoint(t_node *node1, t_node *node2)
 {
-	if (node1 == NULL || node2 == NULL)
-		return (-1);
 	if (to_root(node1) == to_root(node2))
 		return (false);
 	return (true);
@@ -68,8 +64,6 @@ int is_disjoint(t_node *node1, t_node *node2)
 
 void union_node(t_node *node1, t_node *node2)
 {
-	if (node1 == NULL || node2 == NULL)
-		return ;
 	if (is_disjoint(node1, node2) == true)
 		to_root(node2)->parent = to_root(node1);
 }
@@ -78,18 +72,19 @@ int main(void)
 {
 	int N, M;
 	int operation, a, b;
+    int i;
 
 	scanf("%d %d", &N, &M);
-	while (N--)
-		init[N] = create_node(N + 1);
-	while (M--)
+	for (i = 0 ; i <= N ; i++)
+		init[i] = create_node(i);
+    for (i = 0 ; i < M ; i++)
 	{
 		scanf("%d %d %d", &operation, &a, &b);
 		if (operation == UNION)
-			union_node(init[a - 1], init[b - 1]);
+			union_node(init[a], init[b]);
 		else
 		{
-			if (is_disjoint(init[a - 1], init[b - 1]))
+			if (is_disjoint(init[a], init[b]))
 				printf("NO\n");
 			else
 				printf("YES\n");
